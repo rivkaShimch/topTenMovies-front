21fae28e6a96ba282b8e1fb2a32ad0da5836aa4a
Rivka-Fogelman <76700130+Rivka-Fogelman@users.noreply.github.com>
2021-10-12 12:01:15 +0300

    fix add movie form

--- src/components/AddMovie/addMovie.js
+++ src/components/AddMovie/addMovie.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { connect, useDispatch, useSelector } from 'react-redux';
-import {actions} from '../../redux/actions/action'
+import { actions } from '../../redux/actions/action'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col'
@@ -8,48 +8,59 @@ import Row from 'react-bootstrap/Row'
 
 function AddMovie(props) {
     debugger
-       return (<div>
-<Form className="formAddMovie" >
-<Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-    <Form.Label column sm="5">
-    Name
+    return (<div>
+        <Form className="formAddMovie" >
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="5">
+                    Name
     </Form.Label>
-    <Col sm="5">
-      <Form.Control type="text" placeholder="Enter Movie Name" />
-    </Col>
-  </Form.Group>
-  <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-    <Form.Label column sm="5">
-    Movie's cover image
+                <Col sm="5">
+                    <Form.Control type="text" placeholder="Enter Movie Name" />
+                </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="5">
+                    Movie's cover image
     </Form.Label>
-    <Col sm="5">
-      <Form.Control  type="file" placeholder="Choose image" />
-    </Col>
-  </Form.Group>
-  <Form.Select >
-    <option>Choose movie's category</option>
-    <option value="1">Action</option>
-    <option value="2">Drama</option>
-    <option value="3">Comedy</option>
-    <option value="4">Tension</option>
-    <option value="5">Science Fiction</option>
-  </Form.Select>
-  <div className="paddingDiv" style={{padding:"5px"}}></div>
-  <Form.Select>
-    <option>Choose movie's rate</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-  </Form.Select>
-  <div className="paddingDiv" style={{padding:"5px"}}></div>
+                <Col sm="5">
+                    <Form.Control type="file" placeholder="Choose image" />
+                </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="5">
+                    Choose movie's category
+    </Form.Label>
+                <Col sm="5">
+                    <Form.Select>
+                        <option value="0">Choose...</option>
+                        <option value="1">Action</option>
+                        <option value="2">Drama</option>
+                        <option value="3">Comedy</option>
+                        <option value="4">Tension</option>
+                        <option value="5">Science Fiction</option>
+                    </Form.Select>    </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+                <Form.Label column sm="5">
+                    Choose movie's rate
+    </Form.Label>
+                <Col sm="5">
+                    <Form.Select>
+                        <option value="0">Choose...</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </Form.Select>    </Col>
+            </Form.Group>
+            <div className="paddingDiv" style={{ padding: "5px" }}></div>
 
-  <Button variant="primary" type="submit">
-    Submit
+            <Button variant="primary" type="submit">
+                Submit
   </Button>
-</Form>
-        </div>)
+        </Form>
+    </div>)
 
 }
 
